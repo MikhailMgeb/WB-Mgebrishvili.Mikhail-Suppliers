6578466fcb6ad9590bcd0cf5a4c607610983896b
MikhailMgeb <mmd920802@gmail.com>
2024-06-15 18:16:11 +0300

    fix-synt-in-project

--- server/src/middlewares/middlewares.ts
+++ server/src/middlewares/middlewares.ts
@@ -1,5 +1,6 @@
 import type { Request, Response, NextFunction } from 'express';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function errorMiddleware(err: Error, _req: Request, res: Response, _next: NextFunction) {
-    res.status(500).send(err.message);
+  res.status(500).send(err.message);
 }
